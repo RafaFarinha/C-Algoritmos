98b8584e89fe8a69ebcb09d233a0592522c20bb5
Rafael <rafa.skt.life@gmail.com>
2023-03-23 10:54:59 -0300

    Adição de 2 Exercicios C#

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2. Escrever um programa fonte na Linguagem C# que exiba a soma, a subtração, 
+             a multiplicação e a divisão de dois números inteiros fornecidos pelo usuário. */
+
+            double a;
+            double b;
+
+            double soma;
+            double sub;
+            double multi;
+            double divi;
+
+
+            Console.WriteLine("Digite o primeiro valor");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor");
+            b = double.Parse(Console.ReadLine());
+
+            soma = a + b;
+            sub = a - b;
+            multi = a * b;
+            divi = a / b;
+
+            Console.WriteLine();
+            Console.WriteLine("Os resultados são:");
+            Console.WriteLine("Soma = {0}", soma);
+            Console.WriteLine("Subtração = {0}", sub);
+            Console.WriteLine("Multiplicação = {0}", multi);
+            Console.WriteLine("Divisão = {0}", divi);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 3. Escrever um programa fonte na Linguagem C# que leia o nome de um vendedor, 
+             o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
+             Sabendo que esse vendedor ganha 20% de comissão sobre suas vendas efetuadas, 
+             o programa deverá informar o nome do vendedor, o salário fixo e salário no final do mês.*/
+
+            string nomeVendedor;
+            double salario;
+            int totalVendas;
+
+            Console.WriteLine("Olá Vendedor, para darmos inicio no seu processo de calculo da sua comissão, me diga primeiramente seu nome!");
+            nomeVendedor = string input = Console.ReadLine();
+        }
+    }
+}
